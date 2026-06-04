df8f21e7a7f0ef684306e08fe62b1378601268d5
gamelalag <huu2342003@gmail.com>
2026-06-04 10:24:48 +0700

    feat: inittal spring boot project setup

---
+++ src/main/java/com/workspacesharing/WorkspaceSharingApplication.java
@@ -0,0 +1,13 @@
+package com.workspacesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkspaceSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workspacesharing/WorkspaceSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workspacesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
